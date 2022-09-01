6d20d24fdb72a06ae476fc4739a8ee2842022d34
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-09-01 23:33:33 +0800

    cart state

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,19 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categoryId: 0,
+  totalPrice: 0,
+  items: [],
 };
 
-export const filterSlice = createSlice({
+export const cartSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    setCategoryId(state, action) {
-      state.categoryId = action.payload;
+    addItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count--;
+      }
+    },
+
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+    },
+    clearItems(state) {
+      state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { setCategoryId, setSort, setCurrentPage, setFilters } = filterSlice.actions;
+export const { addItem, removeItem, minusItem, clearItems } = cartSlice.actions;
 
-export default filterSlice.reducer;
+export default cartSlice.reducer;
